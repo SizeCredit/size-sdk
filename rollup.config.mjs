e65a7cfdecb7a330adbfc38b994aaa9f929708ec
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-05-28 11:48:20 -0300

    0.0.7

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,59 +1,52 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
 import json from "@rollup/plugin-json";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 const packageJson = require("./package.json");
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        dir: "dist/cjs",
-        format: "cjs",
-        sourcemap: true,
-        exports: "named",
-        esModule: false
-      },
-      {
-        dir: "dist/esm",
-        format: "esm",
-        sourcemap: true,
-        exports: "named",
-        preserveModules: true,
-        preserveModulesRoot: "src"
-      },
-    ],
-    plugins: [
-      resolve({
-        preferBuiltins: true,
-        mainFields: ['module', 'main'],
-        extensions: ['.ts', '.js', '.json']
-      }),
-      commonjs({
-        include: /node_modules/,
-        transformMixedEsModules: true,
-        requireReturnsDefault: 'auto'
-      }),
-      json(),
-      typescript({ 
-        tsconfig: "./tsconfig.json",
-        sourceMap: true,
-        module: "ESNext"
-      }),
-    ],
-    external: [
-      ...Object.keys(packageJson.dependencies || {}),
-      ...Object.keys(packageJson.peerDependencies || {}),
-    ],
-  },
-  {
-    input: "src/index.ts",
-    output: [{ file: "dist/types/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
-];
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      dir: "dist/cjs",
+      format: "cjs",
+      sourcemap: true,
+      exports: "named",
+      esModule: false
+    },
+    {
+      dir: "dist/esm",
+      format: "esm",
+      sourcemap: true,
+      exports: "named",
+      preserveModules: true,
+      preserveModulesRoot: "src"
+    }
+  ],
+  plugins: [
+    resolve({
+      preferBuiltins: true,
+      mainFields: ['module', 'main'],
+      extensions: ['.ts', '.js', '.json']
+    }),
+    commonjs({
+      include: /node_modules/,
+      transformMixedEsModules: true,
+      requireReturnsDefault: 'auto'
+    }),
+    json(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      compilerOptions: { outDir: undefined, declaration: false, declarationMap: false },
+      sourceMap: true,
+      module: "ESNext"
+    })
+  ],
+  external: [
+    ...Object.keys(packageJson.dependencies || {}),
+    ...Object.keys(packageJson.peerDependencies || {})
+  ]
+};
